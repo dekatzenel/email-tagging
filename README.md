6cc88d87ceb5b8d57d59467d234838e121d5e24e
dekatzenel <dekatzenel@gmail.com>
2020-05-05 12:20:07 -0700

    Get Gmail API integration working with Server.java

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # email-tagging
-To run this program, open it in the Intellij IDEA IDE and run main.java.Server.java
+To run this program, open it in the Intellij IDEA IDE and run Server.java
 
 To access this program once it is running, navigate to <localhost:8080>
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,28 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 
 public class Server implements Runnable, AutoCloseable {
@@ -25,7 +46,11 @@ public class Server implements Runnable, AutoCloseable {
 
     @Override
     public void run() {
-        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + "Hello World!";
+        Gmail service = getGmailService("email-tagging");
+        List<Label> labels = getGmailLabels(service, "me");
+
+        String labelsOutput = getLabelsOutput(labels);
+        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + labelsOutput;
         try {
             connect.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
@@ -33,8 +58,82 @@ public class Server implements Runnable, AutoCloseable {
         }
     }
 
+    private String getLabelsOutput(List<Label> labels) {
+        if (labels.isEmpty()) {
+            return "No labels found.";
+        } else {
+            StringBuilder sb = new StringBuilder("Labels:");
+            for (Label label : labels) {
+                sb.append("\n- ");
+                sb.append(label.getName());
+            }
+            return sb.toString();
+        }
+    }
+
+    /**
+     * Get the labels in the user's gmail account
+     * @param service from which to retrieve labels
+     * @param user to retrieve labels for
+     * @return Retrieved labels or an empty list if there are none or there is an IOException while retrieving the labels
+     */
+    private List<Label> getGmailLabels(Gmail service, String user) {
+        try {
+            ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+            return listResponse.getLabels();
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    private Gmail getGmailService(String applicationName) {
+        try {
+            final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            return new Gmail.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
+                    .setApplicationName(applicationName)
+                    .build();
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException("Failed to create Gmail Service", e);
+        }
+    }
+
     @Override
     public void close() throws Exception {
         connect.close();
     }
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = Server.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
 }
