a7de4dea41818301b79f94c18137890ebe8bff4d
dekatzenel <dekatzenel@gmail.com>
2020-05-05 10:57:39 -0700

    Implement basic multithreading

--- src/Server.java
+++ src/Server.java
@@ -3,18 +3,38 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class Server {
 
-    public static void main(String[] args) throws IOException {
+public class Server implements Runnable, AutoCloseable {
+    private final Socket connect;
 
-        ServerSocket server = new ServerSocket(8080);
+    public Server(Socket c) {
+        connect = c;
+    }
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            try (Socket socket = server.accept()) {
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + "Hello World!";
-                socket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
+            try (Server server = new Server(serverSocket.accept())) {
+                Thread thread = new Thread(server);
+                thread.start();
+                thread.join();
             }
         }
     }
 
+    @Override
+    public void run() {
+        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + "Hello World!";
+        try {
+            connect.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        connect.close();
+    }
 }
